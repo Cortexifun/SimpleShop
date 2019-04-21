a75563b21c55af99d3a5d27ade660b3c7ace68a8
Omid <omidtavanaei@gmail.com>
2019-04-22 02:49:45 +0430

    TableView added

---
+++ SimpleShop/SimpleShop/Services/DataServices.swift
@@ -0,0 +1,24 @@
+//
+//  DataServices.swift
+//  SimpleShop
+//
+//  Created by Omid on 22.04.19.
+//  Copyright © 2019 Omid. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png"),
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
