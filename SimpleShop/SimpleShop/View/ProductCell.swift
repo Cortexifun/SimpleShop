1a0baeba66e0c3f88898a0f0ae4bf14ec81640b1
Omid <omidtavanaei@gmail.com>
2019-04-22 03:53:01 +0430

    collection view added

---
+++ SimpleShop/SimpleShop/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  SimpleShop
+//
+//  Created by Omid on 22.04.19.
+//  Copyright © 2019 Omid. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
