d55ab1a7f52c36d0a4796afa321eb64a7071706b
Omid <omidtavanaei@gmail.com>
2019-04-22 00:09:06 +0430

    Project Initiated

---
+++ SimpleShop/SimpleShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleShop
+//
+//  Created by Omid on 22.04.19.
+//  Copyright © 2019 Omid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
